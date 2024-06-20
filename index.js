0c03775a5bbf9f7085e30307480ade1ab2fd4de4
andres <andres_242001@hotmail.com>
2024-06-20 14:11:54 -0500

    origin *

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 // CORS middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Origin", "Content-Type", "Accept", "Authorization"],
     credentials: true,
