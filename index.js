77e89947f49ba6e6b6a2c72f0df23bfdb3ab8ca9
andres <andres_242001@hotmail.com>
2024-07-04 17:52:20 -0500

    added args

--- index.js
+++ index.js
@@ -38,8 +38,10 @@ app.post("/mockup-generator", async (req, res) => {
     const browser = await puppeteer.launch({
       headless: true,
       executablePath: "/usr/bin/chromium-browser",
+
       args: [
         "--no-sandbox",
+        "--disabled-setupid-sandbox",
         "--disable-setuid-sandbox",
         "--disable-dev-shm-usage",
         "--disable-accelerated-2d-canvas",
